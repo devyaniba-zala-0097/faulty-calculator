73d406ac9a0599a38f39db88c6b22a99b3884854
devyaniba-zala-0097 <72312424+devyaniba-zala-0097@users.noreply.github.com>
2020-10-07 11:36:48 +0530

    Revert "Update exe 2.py"

--- exe 2.py
+++ exe 2.py
@@ -1,13 +1,13 @@
                                             #faulty calculater
 
-while (1):
+while (True):
  a = float(input("Enter Your First Number:"))
  c= input("Which Action Do You Wants Enter Here:")
  b = float(input("Enter Your Second Number:"))
 
- re3 = a+b
+ re1 = a+b
  re2 = a*b
- re1 = a/b
+ re3 = a/b
  re4 = a-b
 
  if a==56 and b==9 and c=="+":
@@ -33,9 +33,9 @@ while (1):
 
  else:
     if "+" in c:
-        print(re3)
-    elif "/" in c:
         print(re1)
+    elif "/" in c:
+        print(re3)
     elif "*" in c:
         print(re2)
     elif "-" in c:
@@ -45,16 +45,17 @@ while (1):
 
     A = input("if You Wants T Re Calculate Enter 'Y' Else Enter 'N':")
 
-    if "N"in A:
+    if "n"in A:
 
         print("Thanks .")
         break
-        
-    if "Y" in A:
+    if "y" in A:
+
         print("Then Enjoying !!!")
         continue
 
     else:
+
        print("Error.")
        print("Please Type 'Y' Or 'N' , Nothing Else.")
        break
